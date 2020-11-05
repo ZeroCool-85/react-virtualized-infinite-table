db05630cce653a14f68c8d55964af52ebd90d75d
Michael Holst <michael.holst@kubikx.com>
2020-11-05 21:44:22 +0100

    Updates

---
+++ precache-manifest.2d46dafbac8de50be34f12a424afb1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7f5b1b5873a762415d04c77531ee53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c094ef0f16f45188e51e",
+    "url": "./static/css/main.310640e6.chunk.css"
+  },
+  {
+    "revision": "a6a7c7bc6ae4eee22af3",
+    "url": "./static/js/2.4b82eaff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4b82eaff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c094ef0f16f45188e51e",
+    "url": "./static/js/main.2bb61be3.chunk.js"
+  },
+  {
+    "revision": "027f8a88a5ab83fdc914",
+    "url": "./static/js/runtime-main.76ba65ed.js"
+  }
+]);
